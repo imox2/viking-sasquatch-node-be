6694bf4709261477f766525de01a02b4e9778ab4
Talha Anwar <talhaanwar.anwar@gmail.com>
2021-06-30 12:02:14 +0530

    corrected socket 404 error+cors error from fe

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express")
 const socketIO = require("socket.io")
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+  cors: {
+    origin: '*',
+  }
+});
 const setupRouter = require("./routes/routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -24,8 +28,14 @@ const router = setupRouter(io);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 //Routes
 app.use("/", router);
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Server started")
 });
