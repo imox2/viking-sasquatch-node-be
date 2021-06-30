c5dbfee871638979d86e1f6e767b6f3aa9d50538
Talha Anwar <talhaanwar.anwar@gmail.com>
2021-06-30 15:25:55 +0530

    Removed unused code

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ const router = setupRouter(io);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
 //Routes
 app.use("/", router);
--- routes/routes.js
+++ routes/routes.js
@@ -3,18 +3,6 @@ const router = express.Router();
 const Tree = require("../model/Tree")
 
 const setupRouter = (io) => {
-    router.post("/setup", async (req, res) => {
-        try {
-            const data = req.body["data"];
-            const tree = new Tree();
-            tree.data = JSON.stringify(data);
-            tree.save();
-            res.status(200).send({success: true});
-        } catch(e) {
-            console.log(e);
-            res.status(400).send({success: false});
-        }
-    });
     router.get("/tree", async (req, res) => {
         try {
             const tree = await Tree.findOne({})
